bc1a1f59ab4f20c08c07d67ffff86f71be7910c6
Pashua85 <pavellashuk85@gmail.com>
2021-03-09 16:56:00 +0500

    add districts

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 async function getPlaces(address) {
+  console.log({address})
   const response = await fetch(
     `https://nominatim.openstreetmap.org/search?q=${address}&format=json&polygon_geojson=1&addressdetails=2`,
   );
   return await response.json();
 }
+
+
 
 ymaps.ready(init);
 
@@ -16,32 +19,64 @@ function init(){
   const geoTitleBallon = new ymaps.Balloon(myMap);
   geoTitleBallon.options.setParent(myMap.options);
 
+  function workWithGeoCode(params, event) {
+    getPlaces(params)
+      .then(places => {
+        const placeIndex = places.findIndex(item => item.geojson.type === 'Polygon' || item.geojson.type === 'MultiPolygon');
+        if (placeIndex !== -1 && places[placeIndex].geojson.type === 'Polygon') {
+          polygon = new ymaps.Polygon(places[placeIndex].geojson.coordinates, {interactivityModel: 'default#transparent'});
+          polygon.events.add('click', event => {
+            geoTitleBallon.close();
+            myMap.geoObjects.remove(polygon);
+          })
+          myMap.geoObjects.add(polygon);
+          geoTitleBallon.open(event.get('coords'), places[placeIndex].display_name.split(', ').slice(0,2).join(', '))
+        }
+        if(placeIndex !== -1 && places[placeIndex].geojson.type === 'MultiPolygon') {
+          places[placeIndex].geojson.coordinates.forEach(coords => {
+            let p = new ymaps.Polygon(coords, {interactivityModel: 'default#transparent'});
+            p.events.add('click', event => {
+              geoTitleBallon.close();
+              myMap.geoObjects.remove(p)
+            });
+            myMap.geoObjects.add(p)
+            geoTitleBallon.open(event.get('coords'), places[placeIndex].display_name.split(', ').slice(0,2).join(', '))
+          })
+        }
+      })
+      .catch(err => {
+        console.err(err);
+      })
+  }
+
+  
+
   let polygon;
   myMap.events.add('click', event => {
+    console.log({zoom: myMap._zoom})
     geoTitleBallon.close();
-    if (polygon) {
-      myMap.geoObjects.remove(polygon)
-    }
+    myMap.geoObjects.removeAll();
+
     ymaps.geocode(event.get('coords'), {
-      kind: 'locality',
+      kind: 'district',
       json: true
     })
       .then(res => {
-        const addressLength = myMap._zoom < 8 ? 2 : 3;
-        if (res.GeoObjectCollection.featureMember.length) {
-          const geoObject = res.GeoObjectCollection.featureMember[0].GeoObject;
-          const params = geoObject.metaDataProperty.GeocoderMetaData.Address.formatted.split(', ').slice(0, addressLength).join('+');
-          
-          getPlaces(params)
-            .then(places => {
-              if(places.length) {
-                polygon = new ymaps.Polygon(places[0].geojson.coordinates);
-                polygon.events.add('click', event => {
-                  geoTitleBallon.close();
-                  myMap.geoObjects.remove(polygon);
-                })
-                myMap.geoObjects.add(polygon);
-                geoTitleBallon.open(event.get('coords'), places[0].display_name.split(', ')[0])
+        if (myMap._zoom >= 11 && res.GeoObjectCollection.featureMember.length) {
+          const geoObject = res.GeoObjectCollection.featureMember[res.GeoObjectCollection.featureMember.length - 1].GeoObject;
+          const params = geoObject.metaDataProperty.GeocoderMetaData.Address.formatted;
+          workWithGeoCode(params, event);
+        } else  {
+          ymaps.geocode(event.get('coords'), {
+            kind: 'locality',
+            json: true
+          })
+            .then(response => {
+              const addressLength = myMap._zoom < 8 ? 2 : 3;
+              if (response.GeoObjectCollection.featureMember.length) {
+                const geoObject = response.GeoObjectCollection.featureMember[0].GeoObject;
+                const params = geoObject.metaDataProperty.GeocoderMetaData.Address.formatted.split(', ').slice(0, addressLength).join('+');
+                workWithGeoCode(params, event)
               }
             })
             .catch(err => {
@@ -50,7 +85,7 @@ function init(){
         }
       })
       .catch(err => {
-        console.error(err);
+        console.err(err)
       })
   })
   
